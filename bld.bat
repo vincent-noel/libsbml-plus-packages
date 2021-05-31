70662217b440575882cf37fff373afe4d82c6416
Vincent Noël <contact@vincent-noel.fr>
2021-05-31 16:20:04 +0200

    Trying to invoke g++ in bld.bat

--- bld.bat
+++ bld.bat
@@ -1,14 +1,14 @@
 mkdir build
 cd build
 
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} ^
-      -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib ^
-      -DCMAKE_CXX_COMPILER=${CXX} ^
-      -DCMAKE_C_COMPILER=${CC} ^
+cmake -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
+      -DCMAKE_INSTALL_LIBDIR=%PREFIX%/lib ^
+      -DCMAKE_CXX_COMPILER=g++ ^
+      -DCMAKE_C_COMPILER=gcc ^
       -DCMAKE_CXX_STANDARD_LIBRARIES=-lxml2 ^
       -DWITH_SWIG=OFF ^
-      -DLIBXML_LIBRARY=${PREFIX}/lib ^
-      -DLIBXML_INCLUDE_DIR=${PREFIX}/include/libxml2 ^
+      -DLIBXML_LIBRARY=%PREFIX%/lib ^
+      -DLIBXML_INCLUDE_DIR=%PREFIX%/include/libxml2 ^
       -DENABLE_COMP=ON ^
       -DENABLE_FBC=ON ^
       -DENABLE_GROUPS=ON ^
