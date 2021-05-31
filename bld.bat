04338645db755d03299436de280e651e0d2b750c
Vincent Noël <contact@vincent-noel.fr>
2021-05-31 18:01:51 +0200

    Fix paths, added clang version print

--- bld.bat
+++ bld.bat
@@ -1,14 +1,14 @@
 mkdir build
 cd build
-
+clang-cl --version
 cmake -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-      -DCMAKE_INSTALL_LIBDIR=%PREFIX%/lib ^
+      -DCMAKE_INSTALL_LIBDIR=%LIBRARY_LIB% ^
       -DCMAKE_CXX_COMPILER=clang-cl ^
       -DCMAKE_C_COMPILER=clang-cl ^
       -DCMAKE_CXX_STANDARD_LIBRARIES=-lxml2 ^
       -DWITH_SWIG=OFF ^
-      -DLIBXML_LIBRARY=%PREFIX%/lib ^
-      -DLIBXML_INCLUDE_DIR=%PREFIX%/include/libxml2 ^
+      -DLIBXML_LIBRARY=%LIBRARY_LIB% ^
+      -DLIBXML_INCLUDE_DIR=%LIBRARY_INC%/libxml2 ^
       -DENABLE_COMP=ON ^
       -DENABLE_FBC=ON ^
       -DENABLE_GROUPS=ON ^
