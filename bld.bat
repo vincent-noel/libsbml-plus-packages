6cf8f607c3757f167b12353c6995282f09d924da
Vincent Noël <contact@vincent-noel.fr>
2021-05-31 15:15:05 +0200

    Added conda-verify, added windows bld.bat

---
+++ bld.bat
@@ -0,0 +1,23 @@
+#!/bin/bash
+mkdir build
+cd build
+
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+      -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
+      -DCMAKE_CXX_COMPILER=${CXX} \
+      -DCMAKE_C_COMPILER=${CC} \
+      -DCMAKE_CXX_STANDARD_LIBRARIES=-lxml2 \
+      -DWITH_SWIG=OFF \
+      -DLIBXML_LIBRARY=${PREFIX}/lib \
+      -DLIBXML_INCLUDE_DIR=${PREFIX}/include/libxml2 \
+      -DENABLE_COMP=ON \
+      -DENABLE_FBC=ON \
+      -DENABLE_GROUPS=ON \
+      -DENABLE_LAYOUT=ON \
+      -DENABLE_MULTI=ON \
+      -DENABLE_QUAL=ON \
+      -DENABLE_RENDER=ON \
+      -DENABLE_DISTRIB=ON \
+      ..
+make
+make install
