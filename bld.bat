4e29b39ba883a24f31563a354b73e764fb7769a2
Vincent Noël <contact@vincent-noel.fr>
2021-06-21 11:42:05 +0200

    Commenting actual build for now

--- bld.bat
+++ bld.bat
@@ -1,23 +1,27 @@
 mkdir build
 cd build
+
+
+
+
 %CXX% --version
 clang-cl --version
-cmake -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-      -DCMAKE_INSTALL_LIBDIR=%LIBRARY_LIB% ^
-      -DCMAKE_CXX_COMPILER=clang-cl ^
-      -DCMAKE_C_COMPILER=clang-cl ^
-      -DCMAKE_CXX_STANDARD_LIBRARIES=-lxml2 ^
-      -DWITH_SWIG=OFF ^
-      -DLIBXML_LIBRARY=%LIBRARY_LIB% ^
-      -DLIBXML_INCLUDE_DIR=%LIBRARY_INC%/libxml2 ^
-      -DENABLE_COMP=ON ^
-      -DENABLE_FBC=ON ^
-      -DENABLE_GROUPS=ON ^
-      -DENABLE_LAYOUT=ON ^
-      -DENABLE_MULTI=ON ^
-      -DENABLE_QUAL=ON ^
-      -DENABLE_RENDER=ON ^
-      -DENABLE_DISTRIB=ON ^
-      %SRC_DIR%
-make
-make install
+@REM cmake -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+@REM       -DCMAKE_INSTALL_LIBDIR=%LIBRARY_LIB% ^
+@REM       -DCMAKE_CXX_COMPILER=clang-cl ^
+@REM       -DCMAKE_C_COMPILER=clang-cl ^
+@REM       -DCMAKE_CXX_STANDARD_LIBRARIES=-lxml2 ^
+@REM       -DWITH_SWIG=OFF ^
+@REM       -DLIBXML_LIBRARY=%LIBRARY_LIB% ^
+@REM       -DLIBXML_INCLUDE_DIR=%LIBRARY_INC%/libxml2 ^
+@REM       -DENABLE_COMP=ON ^
+@REM       -DENABLE_FBC=ON ^
+@REM       -DENABLE_GROUPS=ON ^
+@REM       -DENABLE_LAYOUT=ON ^
+@REM       -DENABLE_MULTI=ON ^
+@REM       -DENABLE_QUAL=ON ^
+@REM       -DENABLE_RENDER=ON ^
+@REM       -DENABLE_DISTRIB=ON ^
+@REM       %SRC_DIR%
+@REM make
+@REM make install
