a9c0b8697572e73cfa80fb4e396db984223f3ba6
Vincent Noël <contact@vincent-noel.fr>
2021-06-21 13:36:16 +0200

    Cleanup, return to clang

--- bld.bat
+++ bld.bat
@@ -1,18 +1,10 @@
 mkdir build
 cd build
 
-
-dir 
-
-%CXX%
-clang-cl --version
-cmake --version
-dir %LIBRARY_LIB%
-dir %LIBRARY_INC%
 cmake -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -DCMAKE_INSTALL_LIBDIR=%LIBRARY_LIB% ^
-      -DCMAKE_CXX_COMPILER=%CXX% ^
-      -DCMAKE_C_COMPILER=%CXX% ^
+      -DCMAKE_CXX_COMPILER=clang-cl ^
+      -DCMAKE_C_COMPILER=clang-cl ^
       -DCMAKE_CXX_STANDARD_LIBRARIES=-lxml2 ^
       -DWITH_SWIG=OFF ^
       -DLIBXML_LIBRARY=%LIBRARY_LIB% ^
