2bd8d1f5c5d044381e65cfd8733899f44201b720
Vincent Noël <vincent.noel@curie.fr>
2025-03-20 08:30:30 -0300

    Added BUILD_SHARED_LIBS=ON

--- bld.bat
+++ bld.bat
@@ -4,6 +4,7 @@ cmake -G"Ninja" -S . -B build ^
     -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
     -DCMAKE_INSTALL_LIBDIR="%PREFIX%"/lib ^
     -DCMAKE_BUILD_TYPE=Release ^
+    -DBUILD_SHARED_LIBS=ON ^
     -DCMAKE_CXX_COMPILER="%CXX%" ^
     -DCMAKE_C_COMPILER="%CC%" ^
     -DCMAKE_CXX_STANDARD_LIBRARIES=-lxml2 ^
