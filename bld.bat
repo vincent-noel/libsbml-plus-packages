ea00aca9c20e5422439efbe7fe42d5a9146cac4d
Vincent Noël <vincent.noel@curie.fr>
2025-05-07 15:55:16 -0300

    Improved build scripts

--- bld.bat
+++ bld.bat
@@ -12,11 +12,9 @@ cmake -G"Ninja" -S . -B build ^
     -DWITH_SWIG=OFF ^
     -DLIBXML_LIBRARY="%PREFIX%"/Library/lib/libxml2.lib ^
     -DLIBXML_INCLUDE_DIR="%PREFIX%"/Library/include/libxml2 ^
-    -DENABLE_COMP=ON -DENABLE_FBC=ON -DENABLE_GROUPS=ON ^
-    -DENABLE_LAYOUT=ON -DENABLE_MULTI=ON -DENABLE_QUAL=ON ^
-    -DENABLE_RENDER=ON -DENABLE_DISTRIB=ON -DENABLE_ARRAYS=ON ^
-    -DENABLE_DYN=ON -DENABLE_REQUIREDELEMENTS=ON ^
-    -DENABLE_SPATIAL=ON -DWITH_CPP_NAMESPACE=ON
+    -DWITH_STABLE_PACKAGES=ON ^
+    -DWITH_CPP_NAMESPACE=ON ^
+    -DMISC_PREFIX="%PREFIX%"/Library/share/libsbml/
 
 cmake --build build --parallel %CPU_COUNT% --config Release
 cmake --build build --target install
--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-mkdir build
-cd build
-
 declare -a CMAKE_LIBXML_LIBRARY
 if [[ ${target_platform} == osx-64 ]]; then
   CMAKE_LIBXML_LIBRARY+=(-DLIBXML_LIBRARY="${PREFIX}"/lib/libxml2.dylib)
@@ -9,28 +6,19 @@ elif [[ ${target_platform} == linux-64 ]]; then
   CMAKE_LIBXML_LIBRARY+=(-DLIBXML_LIBRARY="${PREFIX}"/lib/libxml2.so)
 fi
 
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+cmake -B build -S . \
+      -DCMAKE_INSTALL_PREFIX=${PREFIX} \
       -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
       -DCMAKE_BUILD_TYPE=Release \
+      -DBUILD_SHARED_LIBS=ON \
       -DCMAKE_CXX_COMPILER=${CXX} \
       -DCMAKE_C_COMPILER=${CC} \
       -DCMAKE_CXX_STANDARD_LIBRARIES=-lxml2 \
       -DWITH_SWIG=OFF \
       "${CMAKE_LIBXML_LIBRARY[@]}" \
       -DLIBXML_INCLUDE_DIR=${PREFIX}/include/libxml2 \
-      -DENABLE_COMP=ON \
-      -DENABLE_FBC=ON \
-      -DENABLE_GROUPS=ON \
-      -DENABLE_LAYOUT=ON \
-      -DENABLE_MULTI=ON \
-      -DENABLE_QUAL=ON \
-      -DENABLE_RENDER=ON \
-      -DENABLE_DISTRIB=ON \
-      -DENABLE_ARRAYS=ON \
-      -DENABLE_DYN=ON \
-      -DENABLE_REQUIREDELEMENTS=ON \
-      -DENABLE_SPATIAL=ON \
+      -DWITH_STABLE_PACKAGES=ON \
       -DWITH_CPP_NAMESPACE=ON \
       ..
-make -j"${CPU_COUNT}"
-make install
+cmake --build build --parallel "${CPU_COUNT}" --config Release
+cmake --install build
